c296922bf27c8f741039a11669a8f6cce90597d3
MrZhang21626 <1792068425@qq.com>
2023-04-30 17:57:48 +0800

    Update modid

---
+++ ModernTech/src/main/java/com/mrzhang21626/moderntech/ModernTech.java
@@ -0,0 +1,25 @@
+package com.mrzhang21626.moderntech;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(ModernTech.MODID)
+public class ModernTech {
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MODID = "moderntech";
+
+}
