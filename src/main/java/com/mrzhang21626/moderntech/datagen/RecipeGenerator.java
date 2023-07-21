903ba19597a5284ba370ee19e9b47da34e85352f
MrZhang21626 <1792068425@qq.com>
2023-07-21 14:40:53 +0800

    Add dusts recipes

--- src/main/java/com/mrzhang21626/moderntech/datagen/RecipeGenerator.java
+++ src/main/java/com/mrzhang21626/moderntech/datagen/RecipeGenerator.java
@@ -4,6 +4,7 @@ import com.mrzhang21626.moderntech.materials.Material;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
@@ -27,6 +28,34 @@ public class RecipeGenerator extends RecipeProvider {
                         .unlockedBy("has_" + material.name + "plates", has(ItemTags.create(new ResourceLocation("forge", "ingots/" + material.name))))
                         .save(consumer, new ResourceLocation("moderntech", "materials/" + material.name + "_plate_can_process"));
 
+            }
+            if (material.hasDustSeries) {
+                var small_dust = material.ITEMS.get("small_dusts").get();
+                var tiny_dust = material.ITEMS.get("tiny_dusts").get();
+                var dust = material.ITEMS.get("dusts").get();
+                ShapedRecipeBuilder.shaped(dust)
+                        .pattern("##")
+                        .pattern("##")
+                        .define('#', small_dust)
+                        .unlockedBy("has_" + material.name + "_small_dusts", has(ItemTags.create(new ResourceLocation(
+                                "forge", "small_dusts/" + material.name))))
+                        .save(consumer, new ResourceLocation("moderntech", "materials/" + material.name +
+                                "_dusts_from_small_dusts"));
+                ShapedRecipeBuilder.shaped(dust)
+                        .pattern("###")
+                        .pattern("###")
+                        .pattern("###")
+                        .define('#', tiny_dust)
+                        .unlockedBy("has_" + material.name + "_tiny_dusts", has(ItemTags.create(new ResourceLocation(
+                                "forge", "tiny_dusts/" + material.name))))
+                        .save(consumer, new ResourceLocation("moderntech", "materials/" + material.name +
+                                "_dusts_from_tiny_dusts"));
+                ShapelessRecipeBuilder.shapeless(small_dust, 4)
+                        .requires(dust, 1)
+                        .unlockedBy("has_" + material.name + "_dusts", has(ItemTags.create(new ResourceLocation(
+                                "forge", "dusts/" + material.name))))
+                        .save(consumer, new ResourceLocation("moderntech", "materials/" + material.name +
+                                "_small_dusts_from_dusts"));
             }
         }
     }
